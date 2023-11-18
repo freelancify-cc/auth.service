77eb5acc26180bf7ee17b9cd4ead7291b2de4c23
rxOred <rxored@gmail.com>
2023-11-19 00:58:31 +0530

    feat: added constraints

--- migrations/20231117193312_init.up.sql
+++ migrations/20231117193312_init.up.sql
@@ -2,44 +2,44 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE 
-    if NOT EXISTS UserRoles (
+    IF NOT EXISTS UserRoles (
         id INT PRIMARY KEY NOT NULL,
-        role_name VARCHAR(12) NOT NULL,
+        role_name VARCHAR(12) NOT NULL
     );
 
 CREATE TABLE 
-    if NOT EXISTS Users (
+    IF NOT EXISTS Users (
         id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
         email VARCHAR(30) UNIQUE NOT NULL,
         password VARCHAR(20) NOT NULL,
         user_role INT NOT NULL,
-        FOREIGN KEY (user_role) REFERENCES UserRoles(id),
+        CONSTRAINT fk_user_role FOREIGN KEY (user_role) REFERENCES UserRoles(id),
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        upated_at TIMESTAMP NULL,
+        upated_at TIMESTAMP 
     );
 
 CREATE TABLE 
-    if NOT EXISTS UserProfiles (
+    IF NOT EXISTS UserProfiles (
         id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
         first_name VARCHAR(20) NOT NULL,
         second_name VARCHAR(20) NOT NULL,
         date_of_birth TIMESTAMP NOT NULL,
         username VARCHAR(10) NOT NULL,
         profile_picture_url VARCHAR NOT NULL,
-        user UUID NOT NULL,
-        FOREIGN KEY (user) REFERENCES User(id),
+        user_id UUID NOT NULL,
+        CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES Users(id),
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        upated_at TIMESTAMP NULL,
+        upated_at TIMESTAMP 
     );
 
 CREATE TABLE 
-    if NOT EXISTS UserContactInformation (
+    IF NOT EXISTS UserContactInformation (
         id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
-        contact VARHCAR(10) NOT NULL,
-        user_profile UUID NOT NULL,
-        FOREIGN KEY (user_profile) REFERENCES UserProfile(id),
+        contact VARCHAR(10) NOT NULL,
+        user_profile_id UUID NOT NULL,
+        CONSTRAINT fk_user_profile FOREIGN KEY (user_profile_id) REFERENCES UserProfiles(id),
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        upated_at TIMESTAMP NULL,
+        upated_at TIMESTAMP
     );
 
 CREATE TABLE 
@@ -47,8 +47,8 @@ CREATE TABLE
         id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
         bank VARCHAR(20) NOT NULL,
         account VARCHAR NOT NULL,
-        user_profile UUID NOT NULL,
-        FOREIGN KEY (user_profile) REFERENCES UserProfile(id),
+        user_profile_id UUID NOT NULL,
+        CONSTRAINT fk_user_profile FOREIGN KEY (user_profile_id) REFERENCES UserProfiles(id),
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        upated_at TIMESTAMP NULL,
+        upated_at TIMESTAMP
     );
